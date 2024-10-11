cd73735d0dbb6079a701444ebbdb07a220b667ba
KristijanBoshev <154442308+KristijanBoshev@users.noreply.github.com>
2024-10-11 18:19:02 +0200

    display content

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from app.naive.generation import generate
+from app.naive.ingestion import document_ingest
 
 def main():
-    generate_answer = generate.generate_answer()
+   
+    generate_answer = generate._generate_answer()
 
-    print(f'Answer: {generate_answer}')
+    print(f'Answer: {generate_answer.content}')
 
 if __name__ == '__main__':
     main()    
